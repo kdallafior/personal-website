be7ab5aa5e7013d2dc964ddac9d49cebb8e22ba0
Kyle Dallafior <dal@half.rest>
2016-10-06 16:03:01 +0200

    base of the page. Will refactor

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// Pull out due to time
+// var wrap = $("#wrap");
+//
+// wrap.on("scroll", function(e) {
+//
+//   if (this.scrollTop > 1) {
+//     wrap.addClass("hidder");
+//   } else {
+//     wrap.removeClass("hidder");
+//   }
+//
+// });
